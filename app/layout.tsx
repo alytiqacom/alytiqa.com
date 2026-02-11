3b17ec2ae58b6bb9f51adba835af4ddd97162b82
Carl Grundberg <carl.grundberg@menmo.se>
2026-02-11 08:55:41 +0100

    Update route types import, add Matomo script to layout, and remove unused placeholder SVG files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import type { Metadata, Viewport } from 'next'
+import Script from 'next/script'
 import { IBM_Plex_Sans } from 'next/font/google'
 
 import './globals.css'
@@ -27,7 +28,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={ibmPlexSans.variable}>
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+        <Script
+          id="matomo-tag-manager"
+          strategy="beforeInteractive"
+        >
+          {`var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+(function() {
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src='https://m.extellio.com/js/container_7SGuIrt2.js'; s.parentNode.insertBefore(g,s);
+})();`}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }
